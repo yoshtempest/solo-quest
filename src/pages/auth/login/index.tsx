f3b79b124bd6ddff92af0b4861bdbaa94ddb6df8
yoshtempest <marcelobenjaminrocha100@gmail.com>
2025-12-28 18:07:29 -0300

    new: login is finished

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,8 +1,12 @@
 import { Mail } from "lucide-react";
 import { Lock } from "lucide-react";
+import { Eye } from "lucide-react";
+import { EyeOff } from "lucide-react";
+import { useState } from "react";
 import styles from "./styles.module.css";
 
 export default function LoginPage() {
+    const [hidePassword, setHidePassowrd] = useState<boolean>(true);
     return (
         <div className="page">
             <h1>Login</h1>
@@ -25,16 +29,40 @@ export default function LoginPage() {
                     <div className={styles.inputContainer}>
                         <Lock className={styles.icon} />
                         <input
-                        type="password"
+                        type={hidePassword ? "password" : "text"}
                         id="password"
                         name="password"
                         placeholder="Digite sua senha"
                         required />
+                        {hidePassword ? (
+                            <Eye
+                                className={styles.eyeicon}
+                                onClick={() => setHidePassowrd(false)}
+                            />
+                            ) : (
+                            <EyeOff
+                                className={styles.eyeicon}
+                                onClick={() => setHidePassowrd(true)}
+                            />
+                        )}
                     </div>
                     <hr />
-                    <a href="/forgot-password">Esqueceu a senha?</a>
                 </div>
+                <a className={styles.link} href="/forgot-password">- Esqueceu a senha?</a>
+                <img
+                src="/src/assets/character.svg"
+                alt="character"
+                className={styles.character}
+                />
                 <button type="submit">Entrar</button>
+                <div className="rowContainer">
+                    <hr className={styles.lowHr} />
+                    <h2 className={styles.spacingRow}>Ou</h2>
+                    <hr className={styles.lowHr} />
+                </div>
+                <button
+                type="button"
+                onClick={() => window.location.href = '/auth/register'}>Cadastre-se</button>
             </form>
         </div>
     )
