94581deb1012dca672d172aba5ad7bacfa435cf5
marcelo <marcelobenjaminrocha100@gmail.com>
2025-12-31 15:19:32 -0300

    new: xpbar in home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,35 @@
+import styles from './styles.module.css'
+import { CircleFadingArrowUp } from 'lucide-react'
+
 export default function HomePage() {
     return (
         <div className="page">
-            <div>
-                <h2>Chikage</h2>
-                <img
-                    src="/src/assets/character.svg"
-                    alt="character"
-                    className="character"
-                />
+            <div className="rowContainer">
+                <div className="columnContainer">
+                    <h2>Chikage</h2>
+                    <img
+                        src="/src/assets/character.svg"
+                        alt="character"
+                        className={styles.character}
+                    />
+                </div>
+                <div className={styles.characterInfos}>
+                    <h3>Nível: 01</h3>
+                    <h4>Ranque: Frango</h4>
+                    <h4>Ofensiva: 0 dias</h4>
+                    <h4>10 de XP para o próximo nível</h4>
+                    <div className="rowContainer">
+                        <div className={styles.xpBar}>
+                            <CircleFadingArrowUp />
+                        </div>
+                        <h4>Nv.02</h4>
+                    </div>
+
+
+                </div>
+
             </div>
-            <h3>Nível: 01</h3>
-            <h4>Ranque: Frango</h4>
-            <h4>Ofensiva: 0 dias</h4>
-            <h4>10 de XP para o próximo nível</h4>
-            <h4>Nv.02</h4>
+
         </div>
     )
 } 
