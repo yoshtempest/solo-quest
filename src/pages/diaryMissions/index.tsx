ce3d401581997a05d905e0c92292910c93d7f08a
yoshtempest <marcelobenjaminrocha100@gmail.com>
2026-01-02 14:21:57 -0300

    feat: passed diary missions to a constant ts arquive

--- src/pages/diaryMissions/index.tsx
+++ src/pages/diaryMissions/index.tsx
@@ -1,91 +1,9 @@
 import styles from './styles.module.css';
-import IMAGES from "@/core/constants/images"
+import DIARY_MISSIONS from '@/core/constants/missions';
 import { useState } from "react";
 
-type Mission = {
-    id: string;
-    title: string;
-    image: string;
-    total: number;
-    unit: string;
-    completed: boolean;
-    tasks: string[];
-};
-
-const INITIAL_MISSIONS: Mission[] = [
-    {
-        id: "stretch",
-        title: "Alongue-se",
-        image: IMAGES.stretching,
-        total: 5,
-        unit: "min",
-        completed: false,
-        tasks: [
-            "Alongamento lateral do pescoço",
-            "Alongamento de braço cruzado no peito",
-            "Panturrilha em pé (apoiado na parede)"
-        ]
-    },
-    {
-        id: "run",
-        title: "Corra",
-        image: IMAGES.run,
-        total: 10,
-        unit: "km",
-        completed: false,
-        tasks: [
-            "Corrida explosiva",
-            "Corrida contínua (ritmo constante)",
-            "Corrida intervalada"
-        ]
-    },
-    {
-        id: "abs",
-        title: "Abdômen",
-        image: IMAGES.abdominal,
-        total: 10,
-        unit: "min",
-        completed: false,
-
-        tasks: [
-            "Abdominal tradicional",
-            "Prancha",
-            "Elevação de pernas deitado"
-        ]
-    },
-    {
-        id: "pushup",
-        title: "Flexão",
-        image: IMAGES.pushUp,
-        total: 100,
-        unit: "reps",
-        completed: false,
-
-        tasks: [
-            "Flexão tradicional",
-            "Flexão com pegada aberta",
-            "Flexão diamante (pegada fechada)"
-        ]
-    },
-    {
-        id: "lower",
-        title: "Inferiores",
-        image: IMAGES.lower,
-        total: 100,
-        unit: "reps",
-        completed: false,
-
-        tasks: [
-            "Elevação de panturrilha em pé",
-            "Elevação de quadril (ponte)",
-            "Avanço (passada) parado"
-        ]
-    }
-];
-
-
 export default function DiaryPage() {
-    const [missions, setMissions] = useState(INITIAL_MISSIONS);
+    const [missions, setMissions] = useState(DIARY_MISSIONS);
 
     function toggleMission(id: string) {
         setMissions((prev) =>
