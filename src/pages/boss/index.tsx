210a39ca0568a066ecd402e0bb729c1e7bc2a350
marcelo <marcelobenjaminrocha100@gmail.com>
2026-01-03 10:46:01 -0300

    new: background of boss fight

--- src/pages/boss/index.tsx
+++ src/pages/boss/index.tsx
@@ -3,20 +3,18 @@ import { ChevronRight } from 'lucide-react'
 
 export default function BossPage() {
     return (
-        <div className="page">
+        <div className={styles.bossPage}>
             <div>
                 <div className="columnContainer">
-                    <h2 className={styles.textCenter}>Leônidas, o Guerreiro</h2>
-                    <div className={styles.hpBar}>Nv 20</div>
+                    <h3 className={styles.textCenter}>Leônidas, o Guerreiro - Nv 10</h3>
                     <img
                         src="/src/assets/leonidas.svg"
                         alt="Leonidas"
                         className={styles.boss}
                     />
                 </div>
-                <h3 className={styles.textCenter}>Nível recomendado: 20</h3>
-                <h3 className={styles.textCenter}>Recompensa: 500XP</h3>
-                <button>Alterar Dificuldade<ChevronRight/></button>
+                <h3 className={styles.textCenter}>Nível recomendado: 10</h3>
+                <button>Dificuldade: Fácil<ChevronRight/></button>
             </div>
         </div>
     )
