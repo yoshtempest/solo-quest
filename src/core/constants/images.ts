b3cb4d3a1d4f8c0f070435d379f449116332132e
yoshtempest <marcelobenjaminrocha100@gmail.com>
2026-01-05 09:11:20 -0300

    feat: removing desnecessarys imports of images and creating a map to do this

--- src/core/constants/images.ts
+++ src/core/constants/images.ts
@@ -1,40 +1,21 @@
-import home from "@/assets/icons/home.svg";
-import boss from "@/assets/icons/boss.svg";
-import conquers from "@/assets/icons/conquers.svg";
-import profile from "@/assets/icons/profile.svg";
+import.meta.glob;
 
-import bossBadge from "@/assets/icons/boss-badge.svg";
-import guild from "@/assets/icons/guild.svg";
-import stretching from "@/assets/icons/stretching.svg";
-import lower from "@/assets/icons/lower.svg";
-import run from "@/assets/icons/run.svg";
-import planche from "@/assets/icons/planche.svg";
-import pushUp from "@/assets/icons/push-up.svg";
-import abs from "@/assets/icons/abs.svg";
-import addicted from "@/assets/icons/addicted.svg";
-import squat from "@/assets/icons/squat.svg";
-import pushUp2 from "@/assets/icons/push-up2.svg";
-import dips from "@/assets/icons/dips.svg";
-import bar from "@/assets/icons/bar.svg";
 
-const IMAGES = {
-    home,
-    boss,
-    conquers,
-    profile,
-    bossBadge,
-    guild,
-    stretching,
-    lower,
-    run,
-    planche,
-    pushUp,
-    abs,
-    addicted,
-    squat,
-    pushUp2,
-    dips,
-    bar,
-}
+const modules = import.meta.glob(
+  "@/assets/icons/*.svg",
+  { eager: true, import: "default" }
+);
+
+const IMAGES = Object.fromEntries(
+  Object.entries(modules).map(([path, file]) => {
+    const name = path
+      .split("/")
+      .pop()!
+      .replace(".svg", "")
+      .replace(/-([a-z])/g, (_, c) => c.toUpperCase()); // kebab → camelCase
+
+    return [name, file];
+  })
+) as Record<string, string>;
 
 export default IMAGES;
