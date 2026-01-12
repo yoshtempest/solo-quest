7990b4a0dd80eaace8e5618436639bcdd4a4a17f
yoshtempest <marcelobenjaminrocha100@gmail.com>
2026-01-12 12:32:43 -0300

    new: gain xp with missions

--- src/pages/missions/weekly/index.tsx
+++ src/pages/missions/weekly/index.tsx
@@ -3,9 +3,13 @@ import MissionList from "@/components/MissionsList";
 import ReturnHome from "@/components/ReturnHome"
 import Poppup from "@/components/Poppup";
 import { usePopup } from "@/hooks/usePoppup";
+import { usePlayer } from "@/contexts/PlayerProgression";
+
+const WEEKLY_XP = 50;
 
 export default function WeeklyPage() {
     const poppup = usePopup();
+    const { gainXp } = usePlayer();
     return (
         <div className="page">
             <div className='rowContainer'>
@@ -15,6 +19,7 @@ export default function WeeklyPage() {
             <MissionList 
                 missionsData={WEEKLY_MISSIONS}
                 onMissionCompleted={(mission) => {
+                    gainXp(WEEKLY_XP);
                     poppup.open(
                         `Missão "${mission.title}" concluída, você ganhou 50xp`
                     );
