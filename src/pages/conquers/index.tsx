c9926bd37717d46050ed23cb4f987f2efc72b9d1
yoshtempest <marcelobenjaminrocha100@gmail.com>
2026-01-14 13:15:30 -0300

    new: conquer completed style

--- src/pages/conquers/index.tsx
+++ src/pages/conquers/index.tsx
@@ -6,12 +6,15 @@ import { useBossProgressionContext } from "@/contexts/BossProgression";
 
 export default function ConquersPage() {
     const { isDifficultyCompleted } = useBossProgressionContext();
-    const easyCompleted = isDifficultyCompleted("Fácil");
+    const anyCompleted = DIFFICULTY_ORDER.some(difficulty =>
+        isDifficultyCompleted(difficulty)
+    );
+
     return (
         <div className="page">
             <div>
                 <div className={styles.rowContainer}>
-                    <div className={`${styles.moreBig} ${easyCompleted ? styles.completed : ""}`}>
+                    <div className={`${styles.moreBig} ${anyCompleted ? styles.completed : ""}`}>
                         <img
                         src={IMAGES.bossBadge}
                         alt="Chefão"
@@ -20,11 +23,20 @@ export default function ConquersPage() {
                     </div>
                     <div className={styles.columnContainer}>
                         <div className={styles.difficulties}>
-                            {DIFFICULTY_ORDER.map((difficulty) => (
-                                <h4 key={difficulty}>{difficulty}</h4>
-                            ))}
+                            {DIFFICULTY_ORDER.map((difficulty) => {
+                                const completed = isDifficultyCompleted(difficulty);
+
+                                return (
+                                    <h4
+                                        key={difficulty}
+                                        className={`${styles.text} ${completed ? styles.completed : ""}`}
+                                    >
+                                        {difficulty}
+                                    </h4>
+                                );
+                            })}
                         </div>
-                        <div className={`${styles.item} ${easyCompleted ? styles.completed : ""}`}>
+                        <div className={`${styles.item} ${anyCompleted ? styles.completed : ""}`}>
                             <h3>Derrote Leônidas, o Guerreiro no modo {DIFFICULTY_ORDER[0]}</h3>
                         </div>
                     </div>
