c297e28b3ff8109a1473df1e3aeb716e48e788de
marcelo <marcelobenjaminrocha100@gmail.com>
2026-02-02 14:25:09 -0300

    new: add person modal and poppup styles

--- soloquest-frontend/src/pages/profile/index.tsx
+++ soloquest-frontend/src/pages/profile/index.tsx
@@ -1,13 +1,17 @@
 import styles from './styles.module.css';
 import ROUTES from '@/core/constants/routes';
 import { useNavigate } from 'react-router';
-import { LogOut, Plus, UserPlus, Minus } from 'lucide-react';
+import { useState } from "react";
+import { LogOut, Plus, UserPlus, Minus, XSquare } from 'lucide-react';
 import IMAGES from "@/core/constants/images"
 import { useUser } from "@/contexts/User";
 import { useStatsAllocation } from "@/hooks/useStatsAllocation";
 import { usePlayer } from "@/contexts/PlayerProgression";
+import Poppup from "@/components/Poppup";
+import { usePopup } from "@/hooks/usePoppup";
 
 export default function ProfilePage() {
+    const poppup = usePopup();
     const navigate = useNavigate();
     const { user } = useUser();
     const { formatLevel } = usePlayer();
@@ -18,6 +22,22 @@ export default function ProfilePage() {
         decrease,
         confirm,
     } = useStatsAllocation();
+
+    const [isAddFriendOpen, setIsAddFriendOpen] = useState(false);
+    const mockUsers = [
+        { id: 1, username: "ShadowWolf" },
+        { id: 2, username: "LunaX" },
+        { id: 3, username: "IronFist" },
+        { id: 4, username: "Nyx" },
+        { id: 5, username: "Blaze" },
+        { id: 6, username: "Vortex" },
+        { id: 7, username: "RogueStar" },
+        { id: 8, username: "Phantom" },
+        { id: 9, username: "CrimsonHawk" },
+        { id: 10, username: "FrostByte" },
+    ];
+    const [friendName, setFriendName] = useState("");
+
 
     return (
         <div className="page">
@@ -52,7 +72,7 @@ export default function ProfilePage() {
                 </div>
             </button>
             <button>
-                <div className='rowContainer'>
+                <div className='rowContainer' onClick={() => setIsAddFriendOpen(true)}>
                     <UserPlus /> 
                     Adicionar Amigo
                 </div>
@@ -63,6 +83,54 @@ export default function ProfilePage() {
                     Sair
                 </div>
             </button>
+            {isAddFriendOpen && (
+            <div className={styles.overlay}>
+                <div className={styles.modal}>
+                    <div className='rowContainer'>
+                        <input
+                            type="text"
+                            placeholder="Nome do usuário"
+                            value={friendName}
+                            className={styles.input}
+                            onChange={(e) => setFriendName(e.target.value)}
+                        />
+                        <button
+                            className={styles.closeButton}
+                            onClick={() => setIsAddFriendOpen(false)}
+                        >
+                            <XSquare size={40} />
+                        </button>
+                    </div>
+ 
+                    <div className={styles.userList}>
+
+                        {mockUsers.map(user => (
+                            <div
+                                key={user.id}
+                                className={styles.userItem}
+                                onClick={() => setFriendName(user.username)}
+                            >
+                                <img
+                                    src="/src/assets/usericon.svg"
+                                    alt={user.username}
+                                    className={styles.avatar}
+                                />
+                                <h4>{user.username}</h4>
+                            </div>
+                        ))}
+                    </div>
+
+                    <button className={styles.button} onClick={() => poppup.open(`Pedido de amizade enviado para ${friendName}`)}>
+                        Enviar convite
+                    </button>
+                    <Poppup 
+                        message={poppup.message}
+                        visible={poppup.visible}
+                        onClose={poppup.close}
+                    />
+                </div>
+            </div>
+        )}
         </div>
     )
 }
